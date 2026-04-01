1ce95310121eb1468e4a865bbb2533157a5d686b
Dominic Wokorach-O <{dominic.olanya}@mail.com>
2026-04-01 19:30:30 +0100

    Update changed

--- src/components/apps/VSCode.tsx
+++ src/components/apps/VSCode.tsx
@@ -2,7 +2,7 @@ export default function VSCode() {
   return (
     <iframe
       className="size-full bg-[#202020]"
-      src="https://github1s.com/Renovamen/playground-macos/blob/main/README.md"
+      src="https://github1s.com/domwokorach/Portfolio-Wokorach/blob/master/README.md"
       title="VSCode"
     />
   );
